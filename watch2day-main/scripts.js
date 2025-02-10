a0cce5d78c8433c9d5f2567f44ffb5e4001ca8c4
Jhune09 <dalumjhun@gmail.com>
2025-02-10 18:38:07 +0800

    Update scripts.js

--- watch2day-main/scripts.js
+++ watch2day-main/scripts.js
@@ -1,4 +1,4 @@
-const API_KEY = 'lagay nyo api key nyo dito';
+const API_KEY = 'ca39cb07a768776cf3d072c6fa0acf52';
 const MOVIE_ENDPOINTS = [
   'https://vidlink.pro/movie/',
   'https://vidsrc.dev/embed/movie/',
